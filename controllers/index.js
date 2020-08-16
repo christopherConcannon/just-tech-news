4a3b45dd9e26e5adf91b969fd11ee042261a0b95
xtophe xanon <xocon10@gmail.com>
2020-08-16 13:06:11 -0500

    add and populate homepage template

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 // brings in api/index.js
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 // prefixes routes in api/index.js with /api
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
 	res.status(404).end();
